f2d39a552be31ae07dcb0362a1e86ab6dbcf4da0
SAMSAM5.5 <sami.saubion@gmail.com>
2026-04-06 12:09:08 +0200

    refactor: python api for objects transform

--- src/grafyte/__init__.py
+++ src/grafyte/__init__.py
@@ -12,26 +12,175 @@ from __grafyte_internal import TextObject as _NativeTextObject
 from __grafyte_internal import InputManager as _NativeInputManager
 from __grafyte_internal import Key, InputTrigger, Vec2, Direction, Hit
 
+class Vec2Proxy:
+    def __init__(self, getter, setter):
+        self._getter = getter
+        self._setter = setter
+
+    def _get(self):
+        return self._getter()
+
+    def _set(self, v):
+        self._setter(v)
+
+    @property
+    def x(self):
+        return self._get().x
+
+    @x.setter
+    def x(self, value):
+        v = self._get()
+        self._set((value, v.y))
+
+    @property
+    def y(self):
+        return self._get().y
+
+    @y.setter
+    def y(self, value):
+        v = self._get()
+        self._set((v.x, value))
+
+    def __iadd__(self, other: Vec2Like):
+        ox, oy = ensure_vec2f("Vec2Proxy.__iadd__(other=...)", other)
+        v = self._get()
+        self._set((v.x + ox, v.y + oy))
+        return self
+
+    def __isub__(self, other: Vec2Like):
+        ox, oy = ensure_vec2f("Vec2Proxy.__iadd__(other=...)", other)
+        v = self._get()
+        self._set((v.x - ox, v.y - oy))
+        return self
+    def __imul__(self, other: float):
+        v = self._get()
+        self._set((v.x * other, v.y * other))
+        return self
+
+    def __iter__(self):
+        v = self._get()
+        yield v.x
+        yield v.y
+
+    def __repr__(self):
+        v = self._get()
+        return f"Vec2Proxy({v.x}, {v.y})"
+
+class RotProxy:
+    def __init__(self, getter, setter):
+        self._getter = getter
+        self._setter = setter
+
+    def _get(self):
+        return float(self._getter())
+
+    def _set(self, value):
+        self._setter(float(value))
+
+    @property
+    def value(self):
+        return self._get()
+
+    @value.setter
+    def value(self, value):
+        self._set(value)
+
+    def set(self, value):
+        self._set(value)
+        return self
+
+    def __float__(self):
+        return self._get()
+
+    def __int__(self):
+        return int(self._get())
+
+    def __iadd__(self, other):
+        self._set(self._get() + float(other))
+        return self
+
+    def __isub__(self, other):
+        self._set(self._get() - float(other))
+        return self
+
+    def __imul__(self, other):
+        self._set(self._get() * float(other))
+        return self
+
+    def __itruediv__(self, other):
+        self._set(self._get() / float(other))
+        return self
+
+    def __repr__(self):
+        return f"RotProxy({self._get()})"
+
 class Object:
     def __init__(self, native_object: _NativeObject):
         self.__native = native_object
+        self.__pos_proxy = Vec2Proxy(
+            getter=self._get_pos,
+            setter=self._set_pos,
+        )
+        self.__scale_proxy = Vec2Proxy(
+            getter=self._get_scale,
+            setter=self._set_scale
+        )
+        self.__rot_proxy = RotProxy(
+            getter=self._get_rot,
+            setter=self._set_rot,
+        )
+
+    def _get_pos(self):
+        return self.__native.pos
+
+    def _set_pos(self, value: Vec2Like):
+        n_value = ensure_vec2f("Object.__set_pos(value=...)", value)
+        self.__native.move_to(*n_value)
+
+    def _get_scale(self):
+        return self.__native.scale
+
+    def _set_scale(self, value: Vec2Like):
+        n_value = ensure_vec2f("Object.__set_scale(value=...)", value)
+        self.__native.set_scale(*n_value)
+
+    def _get_rot(self):
+        return self.__native.rot
+
+    def _set_rot(self, value: float):
+        self.__native.set_rotation(value)
 
     @property
     def pos(self):
-        return self.__native.pos
+        return self.__pos_proxy
+
+    @pos.setter
+    def pos(self, v: Vec2Like):
+        if v is self.__pos_proxy: return
+        self._set_pos(v)
 
     @property
     def rot(self):
-        return self.__native.rot
+        return self.__rot_proxy
+
+    @rot.setter
+    def rot(self, v: float):
+        if v is self.__rot_proxy: return
+        self._set_rot(v)
 
     @property
     def scale(self):
         return self.__native.scale
 
+    @scale.setter
+    def scale(self, v: Vec2Like):
+        if v is self.__scale_proxy: return
+        self._set_scale(v)
+
     def add_collision_box(self, pos: Vec2Like, size: Vec2Like):
-        pos = ensure_vec2f("Object.add_collision_box(pos=...", pos)
-        size = ensure_vec2f("Object.add_collision_box(size=...", size)
-        self.__native.add_collision_box(*pos, *size)
+        n_pos = ensure_vec2f("Object.add_collision_box(pos=...", pos)
+        n_size = ensure_vec2f("Object.add_collision_box(size=...", size)
+        self.__native.add_collision_box(*n_pos, *n_size)
 
     def collides_with(self, other: Object) -> bool:
         return self.__native.collides_with(other.__native)
@@ -39,41 +188,18 @@ class Object:
     def is_colliding(self) -> bool:
         return self.__native.is_colliding()
 
-    def enable_auto_collides(self):
-        self.__native.enable_auto_collides()
-
-    def move(self, offset: Vec2Like):
-        offset = ensure_vec2f("Object.move(offset=...)", offset)
-        self.__native.move(*offset)
-
-    def move_to(self, pos: Vec2Like):
-        pos = ensure_vec2f("Object.move_to(pos=...)", pos)
-        self.__native.move_to(*pos)
-
-    def rotate(self, angle: float):
-        self.__native.rotate(angle)
-
-    def set_rotation(self, angle: float):
-        self.__native.set_rotation(angle)
-
-    @overload
-    def set_scale(self, scale: Vec2Like) -> None: ...
-
-    @overload
-    def set_scale(self, scale: float) -> None: ...
-
-    def set_scale(self, scale: Union[Vec2Like, float]) -> None:
-        self.__native.set_scale(scale)
+    def enable_auto_collides(self, order: int):
+        self.__native.enable_auto_collides(order)
 
     def use_texture(self, texture_source_path: str, slot: int):
         self.__native.use_texture(texture_source_path, slot)
 
     def set_tint(self, tint: Color, strength: float = 1):
-        tint = ensure_color_normalize("Object.set_tint(tint=...", tint)
-        self.__native.set_tint(*tint, strength)
+        n_tint = ensure_color_normalize("Object.set_tint(tint=...", tint)
+        self.__native.set_tint(*n_tint, strength)
     def set_color(self, color: Color, a: float = 1):
-        color = ensure_color_normalize("Object.set_color(color=...", color)
-        self.__native.set_color(*color, a)
+        n_color = ensure_color_normalize("Object.set_color(color=...", color)
+        self.__native.set_color(*n_color, a)
 
 class TextObject:
     def __init__(self, native_object: _NativeTextObject):
@@ -85,8 +211,8 @@ class TextObject:
         self.__native.set_scale(scale)
 
     def set_color(self, color: Color, a: float = 1):
-        color = ensure_color_normalize("TextObject.set_color(color=...)", color)
-        self.__native.set_color(*color, a)
+        n_color = ensure_color_normalize("TextObject.set_color(color=...)", color)
+        self.__native.set_color(*n_color, a)
 
 class Scene:
     def __init__(self, native_scene: _NativeScene):
@@ -98,8 +224,8 @@ class Scene:
                      layer: int = 0,
                      has_texture: bool = False,
                      shader_source_path: str = "") -> Object:
-        pos = ensure_vec2f("Object(pos=...)", pos)
-        size = ensure_vec2f("Object(size=...)", size)
+        n_pos = ensure_vec2f("Object(pos=...)", pos)
+        n_size = ensure_vec2f("Object(size=...)", size)
 
         if shader_source_path == "":
             if has_texture:
@@ -109,13 +235,13 @@ class Scene:
         else:
             shader_source = shader_source_path
 
-        native_obj = self.__native.spawn_object(*size, shader_source, *pos, has_texture, layer)
+        native_obj = self.__native.spawn_object(*n_size, shader_source, *n_pos, has_texture, layer)
         return Object(native_obj)
 
     def spawn_text_object(self, pos: Vec2Like, text: str, scale: float = 12) -> TextObject:
-        pos = ensure_vec2f("Scene.spawn_text_object(pos=...)", pos)
+        n_pos = ensure_vec2f("Scene.spawn_text_object(pos=...)", pos)
 
-        native_obj = self.__native.spawn_text_object(*pos, text, scale)
+        native_obj = self.__native.spawn_text_object(*n_pos, text, scale)
         return TextObject(native_obj)
 
 class InputManager(_NativeInputManager):
@@ -142,14 +268,9 @@ class Application(_NativeApplication):
 
     def __init__(self, name: str, window_dimensions: Vec2Like):
         super().__init__(name, "@embed/Fonts/Base")
-        window_dimensions = ensure_vec2i("Application(window_dimensions=...)", window_dimensions)
-        super().init(*window_dimensions)
+        n_window_dimensions = ensure_vec2i("Application(window_dimensions=...)", window_dimensions)
+        super().init(*n_window_dimensions)
 
     def set_background_color(self, color: Color):
         r, g, b = ensure_color("Application.set_background_color(color=...)", color)
         super().set_clear_color(r / 255, g / 255, b / 255, 1)
-
-    def add_text(self, text, scale, pos: Vec2Like) -> int:
-        pos = ensure_vec2f("Application.draw_text(pos=...)", pos)
-        return super()._native_add_text(*pos, text, scale)
-
